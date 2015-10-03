d224bde7775f0f0d97b6ee3ee3c01fb952f13760
Serge Rodovnichenko <sergerod@gmail.com>
2015-10-03 20:18:16 +0300

    Autogenerated

---
+++ lib/config/plugin.php
@@ -0,0 +1,10 @@
+<?php
+return array (
+  'name' => 'Yandex.Tableau',
+  'icon' => 'img/yatableau.gif',
+  'version' => '0.1',
+  'vendor' => '670917',
+  'handlers' => 
+  array (
+  ),
+);
---
+++ lib/siteYatableauPlugin.class.php
@@ -0,0 +1,6 @@
+<?php
+
+class siteYatableauPlugin extends sitePlugin
+{
+
+}
