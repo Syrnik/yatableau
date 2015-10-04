b6fa5d46afac88036ff084ee2dfb897c46826bdd
Serge Rodovnichenko <sergerod@gmail.com>
2015-10-05 02:41:29 +0300

    Added uninstall

--- lib/siteYatableauPlugin.class.php
+++ lib/siteYatableauPlugin.class.php
@@ -8,4 +8,11 @@ class siteYatableauPlugin extends sitePlugin
 
         return array('menu_li' => $view->fetch($this->path . '/templates/backend_sidebar_menu_li.html'));
     }
+
+    public function uninstall()
+    {
+        $path = wa()->getDataPath("plugins/yatableau/", true);
+        waFiles::delete($path, true);
+        parent::uninstall();
+    }
 }
